1dc0889e321a584d4587e8122739369ebfad4441
pradeep-orchu <133839085+pradeep-orchu@users.noreply.github.com>
2023-09-19 20:58:41 +0530

    added some screens

---
+++ lib/screens/photographers_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PhotographersScreen extends StatefulWidget {
+  const PhotographersScreen({super.key});
+
+  @override
+  State<PhotographersScreen> createState() => _PhotographersScreenState();
+}
+
+class _PhotographersScreenState extends State<PhotographersScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
